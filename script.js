6420c1391cc3404ef22a88ea0f142e00087922fd
Gustavixgamer <gustavosilva.ghcds@gmail.com>
2026-08-08 20:54:20 -0300

    Add factorial and calculator functionality

--- script.js
+++ script.js
@@ -1,113 +1,215 @@
-let email = null
-let senha = null
-let Csenha = null
-let tx1 = document.getElementById("tx1")
-let tx2 = document.getElementById("tx2")
-let tx3 = document.getElementById("tx3")
+var NumberBar = document.querySelector(".NumberBar")
 
-let button1 = document.getElementById("button1")
-let date = document.getElementById('date1')
 
-function guardar(){
+function fatorial(n) {
+    if (n < 0) return NaN;
 
-    email = document.getElementById("tx1").value;
-    senha = document.getElementById("tx2").value;
-    Csenha = document.getElementById("tx3").value;
-    date = document.getElementById("date1").value.replaceAll("-", "/");
+    let resultado = 1;
 
-    if(senha === Csenha) {
-        alert(
-        "Email: " + email +
-        "\nSenha: " + senha +
-        "\nConfirmar senha: " + Csenha +
-        "\nData de Nascimento: " + date
-    )}
-    
-    else {
-        alert("ERRO: as senhas são diferentes")
+    for (let i = 2; i <= n; i++) {
+        resultado *= i;
     }
+
+    return resultado;
 }
 
-function vizualizar(){
 
-    let senha = document.getElementById("tx2")
-
-    if (senha.type === "password"){
-        senha.type = "text"
-        button2.style.color = "aqua"
-    }
-
-    else {
-        senha.type = "password"
-        button2.style.color = "white"
-    }
+var b1 = document.getElementById("b1")
+b1.onclick = function () {
+    NumberBar.textContent += "1";
 }
 
-button2.onclick = vizualizar
+var b2 = document.getElementById("b2")
+b2.onclick = function () {
+    NumberBar.textContent += "2";
+}
 
-function verificar() {
+var b3 = document.getElementById("b3")
+b3.onclick = function () {
+    NumberBar.textContent += "3"; }
 
+var b4 = document.getElementById("b4")
+b4.onclick = function () {
+    NumberBar.textContent += "4";
+}
 
-    if (tx1.value === "") {
+var b5 = document.getElementById("b5")
+b5.onclick = function () {
+    NumberBar.textContent += "5";
+}
 
-        alert("preencha os espaços obrigatórios")
-        tx1.style.borderColor = "red"
+var b6 = document.getElementById("b6")
+b6.onclick = function () {
+    NumberBar.textContent += "6";
+}
 
-    }
+var b7 = document.getElementById("b7")
+b7.onclick = function () {
+    NumberBar.textContent += "7";
+}
 
-    else if (!tx1.value.includes("@")) {
-        alert("Está faltando o @ no email")
-        tx1.style.borderColor = "red"
-    }
+var b8 = document.getElementById("b8")
+b8.onclick = function () {
+    NumberBar.textContent += "8"; }
 
+var b9 = document.getElementById("b9")
+b9.onclick = function () {
+    NumberBar.textContent += "9";
+}
 
-    else if (tx2.value === "") {
+var b0 = document.getElementById("b0")
+b0.onclick = function () {
+    NumberBar.textContent += "0";
+}
 
-        alert("preencha os espaços obrigatórios")
-        tx2.style.borderColor = "red"
+var bEqual = document.getElementById("bEqual")
+bEqual.onclick = function () {
+    try {
+         let expressao = NumberBar.textContent;
+
+        expressao = expressao.replace(/\^/g, "**");
+        expressao = expressao.replace(/π/g, "Math.PI");
+        expressao = expressao.replace(/e/g, "Math.E");
         
+    
+        
+        expressao = expressao.replace(/(\d+)!/g, function(_, numero) {
+            return fatorial(Number(numero));
+        });
+    
+        expressao = expressao.replace(/\d*√(\d+)/g, function (_, numero) {
+    return Math.sqrt(Number(numero));
+});
 
+        expressao = expressao.replace(/\d*Log₂(\d+)/g, function (_, numero) {
+    return Math.log2(Number(numero));
+});
+
+        
+        expressao = expressao.replace(/\d*Log(\d+)/g, function (_, numero) {
+    return Math.log2(Number(numero));
+});
+
+        expressao = expressao.replace(/\d*Log\(([\d.]+),([\d.]+)\)/g,
+function (_, base, numero) {
+    return Math.log(Number(numero)) / Math.log(Number(base));
+});
+
+        NumberBar.textContent = eval(expressao);
+    } 
+    
+    catch {
+        NumberBar.innerHTML = "Erro";
     }
+};
 
-    else if (tx3.value === "") {
-
-        alert("preencha os espaços obrigatórios")
-        tx3.style.borderColor = "red"
-            
-
-    }
-
-    else if (date1.value === "") {
-        alert("preencha os espaços obrigatórios")
-        date1.style.borderColor = "red"
-    }
-
-    else if (tx2.value.length < 8 || tx3.value.length < 8) {
-        alert("Sua senha tem que conter no minimo 8 digitos")
-    }
-
-
-    else if (((tx2.value.match(/[A-Z]/g) || []).length < 2 || tx3.value.match(/[A-Z]/g) || []).length < 2) {
-        alert("Sua senha deve conter no minimo dois digitos em maiusculo")
-    }
-
-    else {
-        guardar()
-    }
+var bPoint = document.getElementById("bPoint")
+bPoint.onclick = function () {
+    NumberBar.textContent += ".";
 }
 
-button1.onclick = verificar
-
-tx1.onclick = function() {
-    tx1.style.borderColor = "#3498db"
-}
-tx2.onclick = function() {
-    tx2.style.borderColor = "#3498db"
-}
-tx3.onclick = function() {
-    tx3.style.borderColor = "#3498db"
-}
-date1.onclick = function() {
-    date1.style.borderColor = "#3498db"
+var bComma = document.getElementById("bComma")
+bComma.onclick = function () {
+    NumberBar.textContent += ",";
 }
 
+var bSum = document.getElementById("bSum")
+bSum.onclick = function () {
+    NumberBar.textContent += "+";
+}
+
+var bSubtraction = document.getElementById("bSubtraction")
+bSubtraction.onclick = function () {
+    NumberBar.textContent += "-";
+}
+
+var bDel = document.getElementById("bDel")
+bDel.onclick = function () {
+    NumberBar.textContent = NumberBar.textContent.slice(0, -1);
+}
+
+var bC = document.getElementById("bC")
+bC.onclick = function () {
+ NumberBar.textContent = "";
+}
+
+var bDiv = document.getElementById("bDiv")
+bDiv.onclick = function () {
+ NumberBar.textContent += "/";
+}
+
+var bMultiply = document.getElementById("bMultiply")
+bMultiply.onclick = function () {
+ NumberBar.textContent += "*";
+}
+
+var bHigh = document.getElementById("bHigh")
+bHigh.onclick = function () {
+ 
+    "^" === "**"
+    NumberBar.textContent += "^";
+
+}
+
+var bPi = document.getElementById("bPi")
+bPi.onclick = function () {
+ 
+    NumberBar.textContent += "\u03C0";
+
+}
+
+var bParenthesisOpen = document.getElementById("bParenthesisOpen")
+bParenthesisOpen.onclick = function () {
+ 
+    NumberBar.textContent += "(";
+
+}
+
+var bParenthesisClose = document.getElementById("bParenthesisClose")
+bParenthesisClose.onclick = function () {
+ 
+    NumberBar.textContent += ")";
+
+}
+
+var bFactorial = document.getElementById("bFactorial")
+bFactorial.onclick = function () {
+ 
+    NumberBar.textContent += "!";
+
+}
+
+var bE = document.getElementById("bE")
+bE.onclick = function () {
+ 
+    NumberBar.textContent += "e";
+
+}
+
+var bSqrt = document.getElementById("bSqrt")
+bSqrt.onclick = function () {
+ 
+    NumberBar.textContent += "√";
+
+}
+
+var bLog2 = document.getElementById("bLog2")
+bLog2.onclick = function () {
+ 
+    NumberBar.textContent += "Log₂";
+
+}
+
+var bLog10 = document.getElementById("bLog10")
+bLog10.onclick = function () {
+ 
+    NumberBar.textContent += "Log";
+
+}
+
+var bLogₐX = document.getElementById("bLogₐX")
+bLogₐX.onclick = function () {
+ 
+    NumberBar.textContent += "Log("
+
+}
